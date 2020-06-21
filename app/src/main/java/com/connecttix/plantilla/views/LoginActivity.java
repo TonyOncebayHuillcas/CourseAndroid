7ea986feb6ce32b0b50d21f8771a7854dd6fa718
tony.oncebay.huillcas <tony.oncebay.huillcas@gmail.com>
2020-06-20 21:03:26 -0500

    Permisos internet y posicion

--- app/src/main/java/com/connecttix/plantilla/views/LoginActivity.java
+++ app/src/main/java/com/connecttix/plantilla/views/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.connecttix.plantilla.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -10,6 +14,7 @@ import com.connecttix.plantilla.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private  final int REQUEST_ACCESS_FINE = 0;
     TextView holamundo;
     EditText name;
 
@@ -21,5 +26,16 @@ public class LoginActivity extends AppCompatActivity {
         holamundo = (TextView) findViewById(R.id.tv_holamundo);
         holamundo.setText("Texto de prueba");
 
+        /** Permisos de la app **/
+        int accessFinePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        int accessCoarsePermission = ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION);
+        if(accessFinePermission!= PackageManager.PERMISSION_GRANTED &&
+                accessCoarsePermission!=PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION
+                    }, REQUEST_ACCESS_FINE);
+        }
+        /**Fin Permisos de la app**/
     }
 }
--- app/src/main/java/com/connecttix/plantilla/views/MainActivity.java
+++ app/src/main/java/com/connecttix/plantilla/views/MainActivity.java
@@ -2,16 +2,37 @@ package com.connecttix.plantilla.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.BatteryManager;
 import android.os.Bundle;
 
 import com.connecttix.plantilla.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context= getApplicationContext();
+        Intent intent=new Intent();
+
+
+        //onReceive(context,intent);
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
+
 }
